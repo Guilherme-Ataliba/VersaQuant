82c4b4c4061708a8984e22ee4ad5579e01e36151
Guilherme Ataliba <lreis.luiz@gmail.com>
2025-02-16 20:47:06 -0300

    implemented symbolic reconstruction analysis class

--- VersaQT/data_manip.py
+++ VersaQT/data_manip.py
@@ -48,187 +48,171 @@ def getBar(data: pd.DataFrame, threshold: int, kind: str = "tick", volume_col: s
 
 
 # Fractional Differentiation
-def __getWeights(d, size, break_zero=False):
-    """
-        d: Degree of differentation (integer or float)
-        size: Max iteration - summation to infinity
-    """
 
-    w=[1.]
+class FractionalDifferentitation():
 
-    for k in range(1, size):
-        w_ = -w[-1]*(d-k+1)/k
-        w.append(w_)
+    def fit(self, data, column):
+        self.data = data
+        self.column=column
 
-        if break_zero and w_ == 0:
-            break
+        self.d = None
+        self.MFD_d = None
+        
+
+    @staticmethod
+    def __getWeights_FFD(d, thres):
+        # Fixed Frac Diff
+        # thres>0 drops insignificant weights
+        w = [1.]
+        k = 1
+        while abs(w[-1]) >= thres:  
+            w_ = -w[-1] / k * (d - k + 1)
+            w.append(w_)
+            k += 1
+        w = np.array(w[ : : -1]).reshape(-1, 1)[1 : ]  
+        return w
+
+    def fracDiff_FFD(self, d=None, thres = 1e-5, start=0, end=1, interval=10, data=None):
+        """
+        Constant width window (new solution)
+        Note 1: thres determines the cut-off weight for the window
+        Note 2: d can be any positive fractional, not necessarily bounded [0,1].
+        """
+
+        if data is None:
+            data = self.data
+
+        if (d is None) and (self.d is None):
+            print("Calculating optimal differentiation degree")
+            self.d = self.OptimalFFD(start, end, interval, thres)
+            print(f"Optimal d found as {self.d}")
+        
+        if d is None:
+            d = self.d
+
+        w = self.__getWeights_FFD(d, thres)
+
+        width = len(w) - 1
+
+        df = {} 
+        for name in data.columns:
+            seriesF = data[[name]].ffill().dropna()
+            df_ = pd.Series() 
+            for iloc1 in range(width, seriesF.shape[0]):
+                loc0 = seriesF.index[iloc1 - width]
+                loc1 = seriesF.index[iloc1]
+                if not np.isfinite(data.loc[loc1,name]):
+                    continue # exclude NAs
+                
+                df_[loc1] = np.dot(w.T, seriesF.loc[loc0 : loc1])[0, 0]
+                
+            df[name] = df_.copy(deep = True)
+        df = pd.concat(df, axis = 1)
+        return df
     
-    return np.array(w[::-1]).reshape(-1, 1)
-
-
-def __plotWeights(dRange, nPlots, size):
-    w = pd.DataFrame()
-    for d in np.linspace(dRange[0], dRange[1], nPlots):
-        w_ = __getWeights(d, size)
-        w_ = pd.DataFrame(w_, index=range(w_.shape[0])[::-1], columns=[d])
-        w = w.join(w_, how='outer')
-
-    sns.lineplot(w)
-
-
-def __get_weight_loss(d, size):
-    w=[1.]
-
-    for k in range(1, size):
-        w_ = -w[-1]*(d-k+1)/k
-        w.append(w_)
-
-    w_total = np.sum(np.abs(w))
-
-    w_loss = []
-    for k in range(len(w)):
-        w_loss_ = np.sum(np.abs(w[k:]))/w_total
-        w_loss.append(w_loss_)
-
-    # sns.lineplot(x=[k for k in range(len(w))], y=w_loss)
-
-    return w_loss
-
-def fracDiff(series, d, thres = .01):
-    """
-    Increasing width window, with treatment of NaNs (Standard Fracdiff, expanding window)
-    Note 1: For thres=1, nothing is skipped.
-    Note 2: d can be any positive fractional, not necessarily bounded [0,1].
-    """
-    #1) Compute weights for the longest series
-    w = __getWeights(d, series.shape[0]) # each obs has a weight
-    #2) Determine initial calcs to be skipped based on weight-loss threshold
-    w_ = np.cumsum(abs(w)) # cumulative weights
-    w_ /= w_[-1] # determine the relative weight-loss
-    skip = w_[w_ > thres].shape[0]  # the no. of results where the weight-loss is beyond the acceptable value
-    #3) Apply weights to values
-    df = {} # empty dictionary
-    for name in series.columns:
-        # fill the na prices
-        seriesF = series[[name]].ffill().dropna()
-        df_ = pd.Series() # create a pd series
-        for iloc in range(skip, seriesF.shape[0]):
-            loc = seriesF.index[iloc] # find the iloc th obs 
-            
-            test_val = series.loc[loc,name] # must resample if duplicate index
-            if isinstance(test_val, (pd.Series, pd.DataFrame)):
-                test_val = test_val.resample('1m').mean()
-            
-            if not np.isfinite(test_val).any():
-                 continue # exclude NAs
-            try: # the (iloc)^th obs will use all the weights from the start to the (iloc)^th
-                df_.loc[loc]=np.dot(w[-(iloc+1):,:].T, seriesF.loc[:loc])[0,0]
-            except:
-                continue
-        df[name] = df_.copy(deep = True)
-    df = pd.concat(df, axis = 1)
-    return df
-
-def __getWeights_FFD(d, thres):
-    # Fixed Frac Diff
-    # thres>0 drops insignificant weights
-    w = [1.]
-    k = 1
-    while abs(w[-1]) >= thres:  
-        w_ = -w[-1] / k * (d - k + 1)
-        w.append(w_)
-        k += 1
-    w = np.array(w[ : : -1]).reshape(-1, 1)[1 : ]  
-    return w
-
-def fracDiff_FFD(series, d, thres = 1e-5):
-    """
-    Constant width window (new solution)
-    Note 1: thres determines the cut-off weight for the window
-    Note 2: d can be any positive fractional, not necessarily bounded [0,1].
-    """
-    #1) Compute weights for the longest series
-    w = __getWeights_FFD(d, thres)
-    # w = getWeights(d, series.shape[0])
-    #w=getWeights_FFD(d,thres)
-    width = len(w) - 1
-    #2) Apply weights to values
-    df = {} # empty dict
-    for name in series.columns:
-        seriesF = series[[name]].ffill().dropna()
-        df_ = pd.Series() # empty pd.series
-        for iloc1 in range(width, seriesF.shape[0]):
-            loc0 = seriesF.index[iloc1 - width]
-            loc1 = seriesF.index[iloc1]
-            if not np.isfinite(series.loc[loc1,name]):
-                continue # exclude NAs
-            #try: # the (iloc)^th obs will use all the weights from the start to the (iloc)^th
-            df_[loc1] = np.dot(w.T, seriesF.loc[loc0 : loc1])[0, 0]
-            # except:
-            #     continue
-            
-        df[name] = df_.copy(deep = True)
-    df = pd.concat(df, axis = 1)
-    return df
-
-def plotMinFFD(instName):
-    from statsmodels.tsa.stattools import adfuller
-
-    if isinstance(instName, pd.DataFrame):
-        instName.to_csv("temp_df.csv")
-        instName = "temp_df"
-
-    path = ''
-    out = pd.DataFrame(columns= ['adfStat', 'pVal', 'lags', 'nObs', '95% conf', 'corr'])
-    df0 = pd.read_csv(path + instName +'.csv',index_col = 0, parse_dates = True)
-    for d in np.linspace(0, 1, 11):
-        df1 = np.log(df0[['Close']]).resample('1D').last() # downcast to daily obs
-        df2 = fracDiff_FFD(df1, d, thres = .01)
-        corr = np.corrcoef(df1.loc[df2.index, 'Close'], df2['Close'])[0, 1]
-        df2 = adfuller(df2['Close'], maxlag = 1, regression = 'c', autolag = None)
-        out.loc[d] = list(df2[ : 4]) + [df2[4]['5%']] + [corr] # with critical value
-    out.to_csv(path + instName + '_testMinFFD.csv')
-    out[['adfStat', 'corr']].plot(secondary_y = 'adfStat')
-    plt.axhline(out['95% conf'].mean(), linewidth = 1, color = 'r', linestyle = 'dotted')
-    # plt.savefig(path + instName + '_testMinFFD.png')
-    return
-
-def plotMinMFD(instName):
-    from statsmodels.tsa.stattools import adfuller
-
-    if isinstance(instName, pd.DataFrame):
-        instName.to_csv("temp_df.csv")
-        instName = "temp_df"
-
-    path = ''
-    out = pd.DataFrame(columns= ['adfStat', 'pVal', 'lags', 'nObs', '95% conf', 'corr'])
-    df0 = pd.read_csv(path + instName +'.csv',index_col = 0, parse_dates = True)
-    for d in np.linspace(0, 1, 11):
-        df1 = np.log(df0[['Close']]).resample('1D').last() # downcast to daily obs
-        df2 = fracDiff(df1, d, thres = .01)
-        corr = np.corrcoef(df1.loc[df2.index, 'Close'], df2['Close'])[0, 1]
-        df2 = adfuller(df2['Close'], maxlag = 1, regression = 'c', autolag = None)
-        out.loc[d] = list(df2[ : 4]) + [df2[4]['5%']] + [corr] # with critical value
-    out.to_csv(path + instName + '_testMinFFD.csv')
-    out[['adfStat', 'corr']].plot(secondary_y = 'adfStat')
-    plt.axhline(out['95% conf'].mean(), linewidth = 1, color = 'r', linestyle = 'dotted')
-    # plt.savefig(path + instName + '_testMinFFD.png')
-    return
-
-def OptimalMFD(data, start = 0, end = 1, interval = 10, t=0.01, column="price"):
+    def OptimalFFD(self, start = 0, end = 1, interval = 10, t=1e-2):
     
-    for d in np.linspace(start, end, interval):    
-        dfx = fracDiff(data.to_frame(), d, thres = t)
-        if sm.tsa.stattools.adfuller(dfx[column], maxlag=1,regression='c',autolag=None)[1] < 0.05:
-            return d
-    print('no optimal d')
-    return d
+        data = self.data[self.column]
 
-def OptimalFFD(data, start = 0, end = 1, interval = 10, t=1e-5, column="price"):
+        for d in np.linspace(start, end, interval):    
+            dfx = self.fracDiff_FFD(d, thres = t, data=data.to_frame())
+            column = dfx.columns[0]
+            if sm.tsa.stattools.adfuller(dfx[column], maxlag=1,regression='c',autolag=None)[1] < 0.05:
+                self.d = d
+                return d
+        raise ValueError('no optimal d')
     
-    for d in np.linspace(start, end, interval):    
-        dfx = fracDiff_FFD(data.to_frame(), d, thres = t)
-        if sm.tsa.stattools.adfuller(dfx[column], maxlag=1,regression='c',autolag=None)[1] < 0.05:
-            return d
-    print('no optimal d')
-    return d
+    @staticmethod
+    def __getWeightsMFD(d, size, break_zero=False):
+        """
+            d: Degree of differentation (integer or float)
+            size: Max iteration - summation to infinity
+        """
+
+        w=[1.]
+
+        for k in range(1, size):
+            w_ = -w[-1]*(d-k+1)/k
+            w.append(w_)
+
+            if break_zero and w_ == 0:
+                break
+        
+        return np.array(w[::-1]).reshape(-1, 1)
+    
+    def fracDiff_MFD(self, d=None, thres = .01, start=0, end=1, interval=10, data=None):
+        """
+        Increasing width window, with treatment of NaNs (Standard Fracdiff, expanding window)
+        Note 1: For thres=1, nothing is skipped.
+        Note 2: d can be any positive fractional, not necessarily bounded [0,1].
+        """
+
+        if data is None:
+            data = self.data
+        
+        if (d is None) and (self.MFD_d is None):
+            print("Calculating optimal differentiation degree")
+            self.MFD_d = self.OptimalMFD(start, end, interval, thres)
+            print(f"Optimal d found as {self.MFD_d}")
+
+        if d is None:
+            d = self.MFD_d
+        
+        
+        w = self.__getWeights(d, data.shape[0]) 
+        w_ = np.cumsum(abs(w)) 
+        w_ /= w_[-1] #
+        skip = w_[w_ > thres].shape[0] 
+
+        df = {} 
+        for name in data.columns:
+            # fill the na prices
+            seriesF = data[[name]].ffill().dropna()
+            df_ = pd.Series() 
+            for iloc in range(skip, seriesF.shape[0]):
+                loc = seriesF.index[iloc]
+                
+                test_val = data.loc[loc,name] 
+                if isinstance(test_val, (pd.Series, pd.DataFrame)):
+                    test_val = test_val.resample('1m').mean()
+                
+                if not np.isfinite(test_val).any():
+                    continue # exclude NAs
+                try: # the (iloc)^th obs will use all the weights from the start to the (iloc)^th
+                    df_.loc[loc]=np.dot(w[-(iloc+1):,:].T, seriesF.loc[:loc])[0,0]
+                except:
+                    continue
+            df[name] = df_.copy(deep = True)
+        df = pd.concat(df, axis = 1)
+        return df
+    
+    def OptimalMFD(self, start = 0, end = 1, interval = 10, t=0.01):
+        
+        data = self.data[self.column]
+
+        for d in np.linspace(start, end, interval):    
+            dfx = self.fracDiff_MFD(d, thres = t, data=data.to_frame())
+            if sm.tsa.stattools.adfuller(dfx[self.column], maxlag=1,regression='c',autolag=None)[1] < 0.05:
+                self.MFD_d = d
+                return d
+        print('no optimal d')
+        return d
+
+    @staticmethod
+    def __get_weight_loss(d, size):
+        w=[1.]
+
+        for k in range(1, size):
+            w_ = -w[-1]*(d-k+1)/k
+            w.append(w_)
+
+        w_total = np.sum(np.abs(w))
+
+        w_loss = []
+        for k in range(len(w)):
+            w_loss_ = np.sum(np.abs(w[k:]))/w_total
+            w_loss.append(w_loss_)
+
+        # sns.lineplot(x=[k for k in range(len(w))], y=w_loss)
+
+        return w_loss
--- personal_projects/symbolic_reconstruct.py
+++ personal_projects/symbolic_reconstruct.py
@@ -16,11 +16,12 @@ class SymbolicReconstruct():
         self.model = model
         self.multi_solutions = None
 
-        test_path = re.findall(r".*\/", save_path)[0] 
-        if os.path.isdir(test_path):
-            self.save_path = save_path
-        else:
-            raise NotADirectoryError(f"{save_path} is not a valid directory")
+        if save_path:
+            test_path = re.findall(r".*\/", save_path)[0] 
+            if os.path.isdir(test_path):
+                self.save_path = save_path
+            else:
+                raise NotADirectoryError(f"{save_path} is not a valid directory")
         
         
 
@@ -34,7 +35,7 @@ class SymbolicReconstruct():
         imfs = ceemdan(series)  # Decompondo a série
         return imfs
     
-    def fit(self, X, y, y_decompose=None, use_indexes=None, seed=None):
+    def fit(self, X, y, y_decompose=None, seed=None):
         if (X.shape[1] != 1) or (y.shape[1] != 1):
             raise ValueError("Expected a 2D array for X and y.")
         
@@ -48,12 +49,13 @@ class SymbolicReconstruct():
             self.decomposition = self.ceemdan_decompose(self.y, seed)
             self.y_decompose = self.decomposition.copy()
 
+    def set_use_indexes(self, use_indexes):
         self.use_indexes = use_indexes
         if use_indexes is not None:
             self.y_decompose = self.y_decompose[use_indexes]
         
 
-    def reconstruct(self, y_decompose=None, local_path=None):
+    def reconstruct(self, local_path=None):
         solutions = []
         for c, decomp in enumerate(self.y_decompose):
             y = np.c_[decomp]
@@ -80,7 +82,7 @@ class SymbolicReconstruct():
 
         return solutions
     
-    def multiple_reconstruct(self, n_times):
+    def multiple_reconstruct(self, n_times, add_path_name=None):
         
         solutions = []
         for n in range(n_times):
@@ -90,10 +92,23 @@ class SymbolicReconstruct():
 
         self.multi_solutions = solutions
         
-        sol_path = re.findall(r".*\/", self.save_path)[0] + "multi_reconstruct.pkl"
 
-        with open(sol_path, "wb") as file:
-            pickle.dump(solutions, file)
+        if self.save_path:
+            if add_path_name:
+                sol_path = re.findall(r".*\/", self.save_path)[0] + f"multi_reconstruct-{add_path_name}.pkl"
+            else:
+                sol_path = re.findall(r".*\/", self.save_path)[0] + f"multi_reconstruct.pkl"
+
+            with open(sol_path, "wb") as file:
+                pickle.dump(solutions, file)
+
+    def fit_solutions(self, solutions=None, solutions_file=None):
+        if solutions is not None:
+            self.multi_solutions = solutions
+        elif solutions_file is not None:
+            with open(solutions_file, "rb") as file:
+                self.multi_solutions = pickle.load(file)
+
 
     def get_best_reconstruction(self, solutions_file=None):
         """
@@ -133,4 +148,205 @@ class SymbolicReconstruct():
 
 
 
+class ReconstructAnalyse():
+    def __init__(self):
+        self.solutions_df = None
+
+    def createSolutions(self, dir, file_name):
+        files = os.listdir(dir)
+        size = len(file_name)
+
+        ranges = []
+        indexes = []
+        solutions_df = []
+
+        for file in files:
+            if (file[0:size] == file_name):
+                range_ = re.findall(r"-[\d\.]*-", file)[0][1:-1]
+                index = re.findall(r"-\d*\.pkl", file)[0][1:-4]
+
+                ranges.append(range_)
+                indexes.append(index)
+
+                with open(os.path.join(dir, file), "rb") as file_:
+                    solution_ = pickle.load(file_)
+
+                    solu_df = pd.DataFrame(solution_)
+                
+                solu_df["Range"] = range_
+                solu_df["Iter"] = index
+
+                solutions_df.append(solu_df)
+
+        final_df = pd.concat(solutions_df)
+        self.solutions_df = final_df
+        return final_df
+    
+    @staticmethod
+    def time_train_test_split(X, y, train_size=0.8):
+        X_ = X.reshape(-1, )
+        y_ = y.reshape(-1, )
+
+        size = X_.size
+        div_id = int(size*train_size)
+
+        X_train, X_test = X_[:div_id], X_[div_id:]
+        y_train, y_test = y_[:div_id], y_[div_id:]
+
+        return X_train, y_train, X_test, y_test
+            
+    def createSum(self, X, y, solutions_df=None, sympy_var=[smp.symbols("x0")], dir=None, file_name=None):
+        if solutions_df is None:
+            if (self.solutions_df is None):
+                if (dir is not None) and (file_name is not None):
+                    self.createSolutions(dir, file_name)
+                else:
+                    raise ValueError("You must run createSolutions first or inform a solution dataframe")
+            solutions_df = self.solutions_df.copy()
+
+        solutions_all = []
+        for range_ in solutions_df["Range"].unique():
+            X_train, y_train, X_test, y_test = self.time_train_test_split(X, y, float(range_))
+
+            sols_ = solutions_df[solutions_df["Range"] == range_]
+
+            solutions = []
+            for iter_ in sols_["Iter"].unique():
+                sols__ = sols_[sols_["Iter"] == iter_]
+                sum_expr = smp.lambdify(sympy_var, np.sum(sols__["Expr"].values))
+                
+                y_pred = sum_expr(X_test)
+
+                solutions_dict = {
+                    "Iter": iter_,
+                    "Range": range_,
+                    "SumExpr": sum_expr,
+                    "y_pred": y_pred,
+                    "y_test": y_test
+                }
+                solutions.append(solutions_dict)
+
+            solutions_all.append(pd.DataFrame(solutions))
+
+        solutions_df = pd.concat(solutions_all).reset_index(drop=True)
+        self.summ_df = solutions_df
+        return solutions_df
+    
+    def cosine_similarity(self, summ_df=None, n_points=None) -> float:
+        if summ_df is None:
+            if self.summ_df is None:
+                raise ValueError("Yous run createSum or give a summ dataframe")        
+            summ_df = self.summ_df
+
+        cosine_values = []
+        for _, row in summ_df.iterrows():
+
+            if n_points is None:
+                actual = np.array(row["y_test"])
+                predicted = np.array(row["y_pred"])
+            else:
+                actual = np.array(row["y_test"][0:n_points])
+                predicted = np.array(row["y_pred"][0:n_points])
+            
+            dot_product = np.dot(actual, predicted)
+            norm_actual = np.linalg.norm(actual)
+            norm_predicted = np.linalg.norm(predicted)
+            
+            # Avoid division by zero
+            if norm_actual == 0 or norm_predicted == 0:
+                return 0.0  # If either vector is zero, similarity is undefined (return 0)
+            
+            cosine_value = {
+                "Iter": row.Iter,
+                "Range": row.Range,
+                "CosineSimilarity": dot_product / (norm_actual * norm_predicted)
+            }
+            
+            cosine_values.append(cosine_value)
         
+        return pd.DataFrame(cosine_values)
+
+
+    def angle_similarity(self, summ_df = None, n_points=None, derivative=True) -> float:
+        if summ_df is None:
+            if self.summ_df is None:
+                raise ValueError("Yous run createSum or give a summ dataframe")        
+            summ_df = self.summ_df
+        
+        angle_values = []
+        for _, row in summ_df.iterrows():
+            if n_points is None:
+                actual = np.array(row["y_test"])
+                predicted = np.array(row["y_pred"])
+            else:
+                actual = np.array(row["y_test"][0:n_points])
+                predicted = np.array(row["y_pred"][0:n_points])
+            
+            # Cetenring the start of both vectors at the origin
+            actual_zero_centered = actual - actual[0]
+            predicted_zero_centered = predicted - predicted[0]
+            
+            if derivative:
+                angle_actual = np.arctan2(np.gradient(actual_zero_centered), 1)
+                angle_predicted = np.arctan2(np.gradient(predicted_zero_centered), 1)
+            else:
+                angle_actual = np.arctan2(actual_zero_centered, 1)
+                angle_predicted = np.arctan2(predicted_zero_centered, 1)
+
+            direction = (np.mean(angle_actual) *np.mean(angle_predicted))/np.abs(np.mean(angle_actual) *np.mean(angle_predicted))
+
+            angle_value = {
+                "Iter": row.Iter,
+                "Range": row.Range,
+                "AngleReal": angle_actual.mean(),
+                "AnglePred": angle_predicted.mean(),
+                "Agreement": direction,
+                "PredDirection": angle_predicted.mean()/(np.abs(angle_predicted.mean()))
+            }
+
+            angle_values.append(angle_value)
+
+            
+            
+        return pd.DataFrame(angle_values)
+        
+    def similarity_analisis(self, summ_df=None, n_points=None, derivative=True):
+        if summ_df is None:
+            if self.summ_df is None:
+                raise ValueError("Yous run createSum or give a summ dataframe")        
+            summ_df = self.summ_df
+
+        angle_similarity = self.angle_similarity(summ_df, n_points, derivative).drop(["Iter", "Range"], axis=1)
+        cosine_similarity = self.cosine_similarity(summ_df, n_points)
+
+        return pd.concat([cosine_similarity, angle_similarity], axis=1)
+    
+
+    def conclusion_analysis(self, summ_df=None, n_points=None, derivative=True):
+        if summ_df is None:
+            if self.summ_df is None:
+                raise ValueError("Yous run createSum or give a summ dataframe")        
+            summ_df = self.summ_df
+
+        similarity_df = self.similarity_analisis(summ_df, n_points, derivative)
+
+        infos = {}
+        for range_ in similarity_df.Range.unique():
+            df_ = similarity_df[similarity_df.Range == range_]
+            mode = df_.PredDirection.mode()[0]
+
+            prob = df_.PredDirection.value_counts().loc[mode]/(df_.PredDirection.value_counts().loc[1.0] + df_.PredDirection.value_counts().loc[-1.0])
+
+            cosine_mean = df_.CosineSimilarity.mean()
+
+            info = {
+                "Direction": mode,
+                "Probility": prob,
+                "CosineSimilarityMean": cosine_mean
+            }
+
+            infos[range_] = info
+        
+        df = pd.DataFrame(infos).T
+        df.index.name = "Range"
+        return df
